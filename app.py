b58b1c0224089620b3476ded7b35fb83aa5d2d52
Bundit Nuntates <silkyland@gmail.com>
2024-07-07 15:56:04 +0700

    chore: Add flask_cors to requirements.txt and enable CORS in app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
 db = SQLAlchemy(app)
 
